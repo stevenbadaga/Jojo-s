dd1a1dac9d67c64b60a58cee7aec31690a213a9d
IRANKUNDA BADAGA Steven <badagaclass@gmail.com>
2026-08-29 23:04:23 +0200

    Enable premium produce hero visuals

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,11 +5,14 @@ import './index.css'
 import './header-tweaks.css'
 import './admin-premium.css'
 import './products-polish.css'
+import './home-produce-polish.css'
 import { installBrandCleanup } from './utils/brandCleanup.js'
 import { installContactEmailSync } from './utils/contactEmailSync.js'
+import { installHeroProduceUpgrade } from './utils/heroProduceUpgrade.js'
 
 installBrandCleanup()
 installContactEmailSync()
+installHeroProduceUpgrade()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
