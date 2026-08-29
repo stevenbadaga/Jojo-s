04d8083dd58d1c9608136196cd086718a4e4ea50
IRANKUNDA BADAGA Steven <badagaclass@gmail.com>
2026-08-29 22:35:37 +0200

    Apply MarketMet branding cleanup globally

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './header-tweaks.css'
+import { installBrandCleanup } from './utils/brandCleanup.js'
+
+installBrandCleanup()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
