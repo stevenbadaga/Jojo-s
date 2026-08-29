c9a1e4833bb4dafd342953c1c6a75a96092f42d9
IRANKUNDA BADAGA Steven <badagaclass@gmail.com>
2026-08-29 23:01:29 +0200

    Keep contact email synced with admin email

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,8 +6,10 @@ import './header-tweaks.css'
 import './admin-premium.css'
 import './products-polish.css'
 import { installBrandCleanup } from './utils/brandCleanup.js'
+import { installContactEmailSync } from './utils/contactEmailSync.js'
 
 installBrandCleanup()
+installContactEmailSync()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
