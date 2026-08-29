c7a579e41826b1aa458a23f92e64c9b0a4ee0c96
IRANKUNDA BADAGA Steven <badagaclass@gmail.com>
2026-08-29 22:53:58 +0200

    Load shared premium admin styling

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './header-tweaks.css'
+import './admin-premium.css'
 import { installBrandCleanup } from './utils/brandCleanup.js'
 
 installBrandCleanup()
@@ -12,4 +13,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
-
